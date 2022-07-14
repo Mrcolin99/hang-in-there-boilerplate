b0b360556c6d8e308851ef336d9be0c99fc01444
Colin Ciervo <colinciervo@@gmail.com>
2022-07-14 18:39:03 -0400

    First steps

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // query selector variables go here 👇
-
+var randomImage = document.querySelector(".show-random");
 // we've provided you with some data to work with 👇
 var images = [
   "./assets/bees.jpg",
@@ -99,13 +99,12 @@ var quotes = [
   "A champion is defined not by their wins but by how they can recover when they fall."
 ];
 var savedPosters = [];
-var currentPoster;
+var currentPoster ;
 
 // event listeners go here 👇
-
+randomImage.addEventListener("click", getRandomIndex());
 // functions and event handlers go here 👇
 // (we've provided one for you to get you started)!
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
-
